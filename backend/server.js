6cf3f0dce6f00ddf59b43f6ff994dc43419bf5f5
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-08-28 14:09:36 +0545

    server updated

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app,server } from "./lib/socket.js";
+import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config()
 // const app = express();
 
@@ -15,13 +17,26 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({limit:"20mb"}));
 app.use(cookieParser());
+if (process.env.NODE_ENV === "development") {
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
-}))
+}));
+}
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV==="production")
+{
+    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+    app.get(/.*/, (req,res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 
 // app.listen(PORT, () => {
